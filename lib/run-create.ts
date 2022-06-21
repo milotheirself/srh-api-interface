66e6fb6184fa4c618cc2326983588176ca32be9b
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-06-21 15:06:14 +0200

    * draft-up

--- lib/run-create.ts
+++ lib/run-create.ts
@@ -109,62 +109,62 @@ database.whenConnected().then(async () => {
     );
   `);
 
-  // // ---
-  // // + append default values
-  // await database.execute(`
-  //   INSERT INTO ${db.name}.${db.data.member}
-  //     (id,                    joined_at,  name,      system) values
-  //     ('298726187285348353',  now(),      'Alice',   false ),
-  //     ('298726222848851970',  now(),      'Glados',  true  ),
-  //     ('298726151784759296',  now(),      'Nelly',   false );
-  // `);
+  // ---
+  // + append default values
+  await database.execute(`
+    INSERT INTO ${db.name}.${db.data.member}
+      (id,                    joined_at,  username, caption, system) values
+      ('298726187285348353',  now() - INTERVAL 100 DAY,      'Alice',  '',      false ),
+      ('298726222848851970',  now(),      'Glados', '',      true  ),
+      ('298726151784759296',  now(),      'Nelly',  '',      false );
+  `);
 
-  // await database.execute(`
-  //   INSERT INTO ${db.name}.${db.data.member}
-  //     (id,                    name   ) values
-  //     ('298726187284621235',  'Gruppe 01'),
-  //     ('298726222848651285',  'Gruppe 02');
-  // `);
+  await database.execute(`
+    INSERT INTO ${db.name}.${db.data.groups}
+      (id,                    name       ) values
+      ('298726187284621235',  'Gruppe 01'),
+      ('298726222848651285',  'Gruppe 02');
+  `);
 
-  // await database.execute(`
-  //   INSERT INTO ${db.name}.${db.data.figure}
-  //     (hash,                                size,   data ) values
-  //     ('64d890648f7b31e8841c4441e5d8625c',  '512',  '...'),
-  //     ('64d890648f7b31e8841c4441e5d8625c',  '256',  '...'),
-  //     ('64d890648f7b31e8841c4441e5d8625c',  '128',  '...'),
+  await database.execute(`
+    INSERT INTO ${db.name}.${db.data.figure}
+      (hash,                                size,   data ) values
+      ('64d890648f7b31e8841c4441e5d8625c',  '512',  '...'),
+      ('64d890648f7b31e8841c4441e5d8625c',  '256',  '...'),
+      ('64d890648f7b31e8841c4441e5d8625c',  '128',  '...'),
 
-  //     ('31e5e4f2ed037db75390982312c3af9f',  '512',  '...'),
-  //     ('31e5e4f2ed037db75390982312c3af9f',  '256',  '...'),
-  //     ('31e5e4f2ed037db75390982312c3af9f',  '128',  '...'),
+      ('31e5e4f2ed037db75390982312c3af9f',  '512',  '...'),
+      ('31e5e4f2ed037db75390982312c3af9f',  '256',  '...'),
+      ('31e5e4f2ed037db75390982312c3af9f',  '128',  '...'),
 
-  //     ('70cb8c5e6af3fa1b933fe1c3ff7d41a3',  '512',  '...'),
-  //     ('70cb8c5e6af3fa1b933fe1c3ff7d41a3',  '256',  '...'),
-  //     ('70cb8c5e6af3fa1b933fe1c3ff7d41a3',  '128',  '...'),
+      ('70cb8c5e6af3fa1b933fe1c3ff7d41a3',  '512',  '...'),
+      ('70cb8c5e6af3fa1b933fe1c3ff7d41a3',  '256',  '...'),
+      ('70cb8c5e6af3fa1b933fe1c3ff7d41a3',  '128',  '...'),
 
-  //     ('46e6b522cbf351c41de1ed1fae9ba3a2',  '512',  '...'),
-  //     ('46e6b522cbf351c41de1ed1fae9ba3a2',  '256',  '...'),
-  //     ('46e6b522cbf351c41de1ed1fae9ba3a2',  '128',  '...'),
+      ('46e6b522cbf351c41de1ed1fae9ba3a2',  '512',  '...'),
+      ('46e6b522cbf351c41de1ed1fae9ba3a2',  '256',  '...'),
+      ('46e6b522cbf351c41de1ed1fae9ba3a2',  '128',  '...'),
 
-  //     ('1a9ffd58f73deb082c59b6062a607b7e',  '512',  '...'),
-  //     ('1a9ffd58f73deb082c59b6062a607b7e',  '256',  '...'),
-  //     ('1a9ffd58f73deb082c59b6062a607b7e',  '128',  '...');
-  // `);
+      ('1a9ffd58f73deb082c59b6062a607b7e',  '512',  '...'),
+      ('1a9ffd58f73deb082c59b6062a607b7e',  '256',  '...'),
+      ('1a9ffd58f73deb082c59b6062a607b7e',  '128',  '...');
+  `);
 
-  // await database.execute(`
-  //   INSERT INTO ${db.name}.${db.link.member_avatar}
-  //     (member,                  figure                              ) values
-  //     ('298726151784759296',  '64d890648f7b31e8841c4441e5d8625c'),
-  //     ('298726187285348353',  '31e5e4f2ed037db75390982312c3af9f'),
-  //     ('298726222848851970',  '70cb8c5e6af3fa1b933fe1c3ff7d41a3');
-  // `);
+  await database.execute(`
+    INSERT INTO ${db.name}.${db.link.member_avatar}
+      (member,                figure                            ) values
+      ('298726151784759296',  '64d890648f7b31e8841c4441e5d8625c'),
+      ('298726187285348353',  '31e5e4f2ed037db75390982312c3af9f'),
+      ('298726222848851970',  '70cb8c5e6af3fa1b933fe1c3ff7d41a3');
+  `);
 
-  // await database.execute(`
-  //   INSERT INTO ${db.name}.${db.link.member_banner}
-  //     (member,                  figure                              ) values
-  //     ('298726187285348353',  '46e6b522cbf351c41de1ed1fae9ba3a2'),
-  //     ('298726222848851970',  '1a9ffd58f73deb082c59b6062a607b7e');
-  // `);
-  // // ---
+  await database.execute(`
+    INSERT INTO ${db.name}.${db.link.member_banner}
+      (member,                figure                            ) values
+      ('298726187285348353',  '46e6b522cbf351c41de1ed1fae9ba3a2'),
+      ('298726222848851970',  '1a9ffd58f73deb082c59b6062a607b7e');
+  `);
+  // ---
 
   database.disconnectedCallback();
 });
