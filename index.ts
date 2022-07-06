73c4646fd252685ebc293a21be52bd29713b7dcd
milotheirself <milotheirself@users.noreply.github.com>
2022-07-06 11:57:01 +0000

    deploy: e3a31509452f5397e9b480d9849f92c9381dbdee

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
+import staticFiles from 'https://deno.land/x/static_files@1.1.6/mod.ts';
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
+const app = new Application();
+
+app.use(staticFiles('./'));
+app.use(async ({ request, response }, next) => {
+  const fil = request.url.toString().split('/').pop();
+
+  if (fil === undefined || fil.includes('.')) {
+    next();
+  } else if (!request.url.toString().endsWith('/')) {
+    response.redirect(`${request.url.toString()}/`);
+  } else {
+    response.status = 404;
+    response.body = await Deno.readTextFile(`${Deno.cwd()}/404.html`);
+  }
 });
+
+await app.listen();
--- lib/.draft/worker/api/index.ts
+++
@@ -1,4 +0,0 @@
-import { default as patch } from './patch.ts';
-import { default as query } from './query.ts';
-
-export default { patch, query };
