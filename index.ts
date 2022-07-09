e34bcd5341facf0b31a9d77b45691bc746b1eb7d
milotheirself <milotheirself@users.noreply.github.com>
2022-07-09 08:26:33 +0000

    deploy: 7114f75475c14a4847144e41331ef8e30dbc95be

--- index.ts
+++ index.ts
@@ -1,71 +1,84 @@
-import { bold, cyan, green, yellow } from 'https://deno.land/std@0.140.0/fmt/colors.ts';
-import { Application, Router, RouterContext } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
+import { Application, Router, isHttpError, Status, Context } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 import staticFiles from 'https://deno.land/x/static_files@1.1.6/mod.ts';
+import { resolve } from 'https://deno.land/std@0.146.0/path/mod.ts';
+
+async function notFound(context: Context) {
+  context.response.status = 404;
+  context.response.type = 'text/html';
+  context.response.body = await Deno.readTextFile(resolve(Deno.cwd(), './404.html'));
+}
+
+function notFoundAPI(context: Context) {
+  context.response.status = Status.NotFound;
+  context.response.type = 'application/json';
+  context.response.body = JSON.stringify({ message: '404: Not Found', code: 0 });
+}
 
 const app = new Application();
 
-// ? logger
-app.use(async ({ response, request }, next) => {
-  await next();
-  const rt = response.headers.get('X-Response-Time');
+// ? error handler
+app.use(async (context, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (isHttpError(err)) {
+      context.response.status = err.status;
+      const { message, status, stack } = err;
 
-  console.log(`${green(request.method)} ${cyan(decodeURIComponent(request.url.pathname))} - ${bold(String(rt))}`);
-});
-
-// ? router
-const router = new Router();
-router
-  .get('/api', ({ response }) => {
-    response.status = 404;
-    response.type = 'application/json';
-    response.body = { message: '404: Not Found', code: 0 };
-  })
-  .get('/api/users/:uid/:role', ({ response, params }) => {
-    if (params.uid == '@me') {
-      // current user id
+      if (context.request.accepts('json')) {
+        context.response.body = { message, status, stack };
+        context.response.type = 'json';
+      } else {
+        context.response.body = `${status} ${message}\n\n${stack ?? ''}`;
+        context.response.type = 'text/plain';
+      }
+    } else {
+      console.log(err);
+      throw err;
     }
-
-    response.status = 404;
-    response.type = 'application/json';
-    response.body = { message: '404: Not Found', code: 0 };
-  })
-  .get('/api/:role/:resource', ({ response, params }) => {
-    console.log({ params });
-
-    response.status = 404;
-    response.type = 'application/json';
-    response.body = { message: '404: Not Found', code: 0 };
-  })
-  .get('/cdn/:role/:resource', ({ response, params }) => {
-    console.log({ params });
-
-    response.status = 404;
-    response.type = 'application/json';
-    response.body = { message: '404: Not Found', code: 0 };
-  });
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-// ? files
-app.use(staticFiles('./'));
-app.use(async ({ request, response }, next) => {
-  const fil = request.url.toString().split('/').pop();
-
-  if (fil === undefined || fil.includes('.')) {
-    next();
-  } else if (!request.url.toString().endsWith('/')) {
-    response.redirect(`${request.url.toString()}/`);
-  } else {
-    response.status = 404;
-    response.type = 'text/html';
-    response.body = await Deno.readTextFile(`${Deno.cwd()}/404.html`);
   }
 });
 
-// ?
+// ? api
+const routerAPI = new Router();
+routerAPI //
+  .get('/api/users/:uid/:role', (context) => {
+    if (context.params.uid == '@me') {
+      // current user id
+    }
+
+    notFoundAPI(context);
+  })
+  .get('/api/:role/:resource', (context) => {
+    console.log({ ...context.params });
+
+    notFoundAPI(context);
+  })
+  .get('/api(.*)', (context) => notFoundAPI(context));
+
+app.use(routerAPI.routes());
+app.use(routerAPI.allowedMethods());
+
+// ? cdn
+const routerCDN = new Router();
+routerCDN //
+  .get('/cdn/:role/:resource', (context) => {
+    console.log({ ...context.params });
+
+    notFound(context);
+  })
+  .get('/cdn(.*)', (context) => notFound(context));
+
+app.use(routerCDN.routes());
+app.use(routerCDN.allowedMethods());
+
+// ? 404
+app.use(staticFiles('./', { redirect: true }));
+app.use(notFound);
+
+// ? listen
 app.addEventListener('listen', ({ hostname, port, secure }) => {
-  console.log(`Listening on: ${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'}:${port}`);
+  console.log('[stream]', `Listening on: ${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'}:${port}`);
 });
 
-await app.listen('localhost:443');
+await app.listen({ port: 443 });
