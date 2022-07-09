7114f75475c14a4847144e41331ef8e30dbc95be
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-07-09 10:26:40 +0200

    refactor: routing

--- packages/insterface-tools/run-stream.ts
+++ packages/insterface-tools/run-stream.ts
@@ -1,27 +1,26 @@
-(async () => {
-  let ser: Deno.Process;
-  let tim: ReturnType<typeof setTimeout>;
+let ser: Deno.Process;
+let tim: ReturnType<typeof setTimeout>;
 
-  function requestBundle() {
-    clearTimeout(tim);
-    tim = setTimeout(async () => {
-      // ?
-      const p = Deno.run({
-        cmd: ['deno', 'run', '-A', '--unstable', 'packages/insterface-tools/run-bundle.ts'], //
-      });
-      await p.status();
+function requestBundle() {
+  clearTimeout(tim);
 
-      // ?
-      if (ser) ser.kill('SIGINT');
-      ser = Deno.run({
-        cwd: 'docs/.pagelet/gh-production/',
-        cmd: ['deno', 'run', '--allow-net=:443', '--allow-read=./', 'index.ts'], //
-      });
-    }, 1000);
-  }
+  tim = setTimeout(async () => {
+    // ?
+    const p = Deno.run({
+      cmd: ['deno', 'run', '-A', '--unstable', 'packages/insterface-tools/run-bundle.ts'], //
+    });
+    await p.status();
 
+    // ?
+    if (ser) ser.kill('SIGINT');
+    ser = Deno.run({
+      cwd: 'docs/.pagelet/gh-production/',
+      cmd: ['deno', 'run', '--allow-net=:443', '--allow-read=./', 'index.ts'], //
+    });
+  }, 1000);
+}
+
+requestBundle();
+for await (const _ of Deno.watchFs('./packages/insterface-content')) {
   requestBundle();
-  for await (const _ of Deno.watchFs('./packages/insterface-content')) {
-    requestBundle();
-  }
-})();
+}
